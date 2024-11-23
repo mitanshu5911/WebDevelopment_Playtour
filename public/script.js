275c80e00b30d4009aec87a0d6551df5ad4f227c
mitanshu5911 <mitanshubansal1234@gmail.com>
2024-11-23 12:11:58 +0530

    Signup connetcivity is done

--- public/script.js
+++ public/script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
    
-        
+    $("#btnSignup").click(function(){
+        let email=$("#signupTxtEmail").val();
+        let password=$("#signupTxtPwd").val();;
+        let usertype=$("#userType").val();
+
+        let obj={
+            type:"get",
+            url:"/signup",
+            data:{
+                txtEmail:email,
+                txtPwd:password,
+                utype:usertype
+            }
+        }
+        $.ajax(obj).done(function(response){
+            alert(response);
+        }).fail(function(err){
+            alert("Server error :"+err.message);
+        })
+    })    
+
 })
